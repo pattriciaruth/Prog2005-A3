7aac3357e7fffecb8974cd705abeea8a81eef4f7
Pattricia Ruth Das <p.das.11@student.scu.edu.au>
2025-11-27 22:43:32 +0800

    rest working fine

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,6 +6,7 @@ import { CafeItem } from '../models/cafe-item';
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
+  standalone: false,
 })
 export class Tab1Page implements OnInit {
 
