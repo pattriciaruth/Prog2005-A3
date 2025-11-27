26ea2df4d1216d367ac332b28089a398189bfd47
Pattricia Ruth Das <p.das.11@student.scu.edu.au>
2025-11-27 22:31:00 +0800

    testing 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InventoryService } from '../services/inventory.service';
+import { CafeItem } from '../models/cafe-item';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  standalone: false,
 })
-export class Tab1Page {
-errorMessage: any;
-items: any;
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  items: CafeItem[] = [];         // 👈 important: initialised as []
+  errorMessage = '';
 
+  constructor(private inventoryService: InventoryService) {}
+
+  ngOnInit() {
+    this.loadItems();
+  }
+
+  loadItems() {
+    this.inventoryService.getItems().subscribe({
+      next: (data) => {
+        console.log('Items from server:', data);
+        this.items = data || [];  // 👈 extra safety
+      },
+      error: (err) => {
+        console.error('Error loading items', err);
+        this.errorMessage = 'Could not load items from server.';
+      }
+    });
+  }
 }
