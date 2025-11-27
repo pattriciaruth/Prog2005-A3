d748d1f6d071ee08d7d18acf59be3b710fd1bc07
Pattricia Ruth Das <p.das.11@student.scu.edu.au>
2025-11-27 21:55:38 +0800

    connected module to inventory service

---
+++ src/app/services/inventory.service.ts
@@ -0,0 +1,57 @@
+// src/app/services/inventory.service.ts
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CafeItem } from '../models/cafe-item';
+
+// Server endpoint given in the assignment brief
+const BASE_URL = 'https://prog2005.it.scu.edu.au/ArtGalley';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InventoryService {
+
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Get all café items.
+   * GET /
+   */
+  getItems(): Observable<CafeItem[]> {
+    return this.http.get<CafeItem[]>(`${BASE_URL}/`);
+  }
+
+  /**
+   * Get a single café item by name.
+   * GET /name
+   */
+  getItemByName(name: string): Observable<CafeItem> {
+    return this.http.get<CafeItem>(`${BASE_URL}/${name}`);
+  }
+
+  /**
+   * Add a new café item.
+   * POST /
+   */
+  addItem(item: CafeItem): Observable<any> {
+    return this.http.post(`${BASE_URL}/`, item);
+  }
+
+  /**
+   * Update an existing café item by name.
+   * PUT /name
+   */
+  updateItem(name: string, item: CafeItem): Observable<any> {
+    return this.http.put(`${BASE_URL}/${name}`, item);
+  }
+
+  /**
+   * Delete a café item by name.
+   * DELETE /name
+   */
+  deleteItem(name: string): Observable<any> {
+    return this.http.delete(`${BASE_URL}/${name}`);
+  }
+}
